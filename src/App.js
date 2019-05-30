f9658994699329c29ae486ee64d37d6ea5a11177
Maria <mariagiraldo4@gmail.com>
2019-05-30 16:37:20 -0500

    Added setting for router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux'
-import './App.css';
+import { Router,Route, Switch } from 'react-router-dom'
 
 import configureStore from './store'
 import Movies from './components/Movies'
+import history from './history'
 
 const store = configureStore()
 
 function App() {
   return (
     <Provider store={store}>  
-      <Movies />
+      <Router history = {history}>
+        <Switch>
+          <Route exact path='/' component={Movies} />
+          <Route exact path='/movies' component={Movies} />
+        </Switch>
+      </Router>     
     </Provider>
   );
 }
